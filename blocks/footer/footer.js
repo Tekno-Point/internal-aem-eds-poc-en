3b4865258eeba54e1f37b2e22b1cf37acb9bb193
pashtepooja18 <poojapashte18@gmail.com>
2025-03-24 17:28:51 +0530

    footer js added

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -18,20 +18,3 @@ export default async function decorate(block) {
 
   block.append(footer);
 }
-
-// footer accordion
-const acc = document.querySelectorAll('.footer-second-container > .default-content-wrapper > ul > li > p');
-let i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    const panel = this.nextElementSibling;
-    // console.log(panel)
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
-  });
-}
