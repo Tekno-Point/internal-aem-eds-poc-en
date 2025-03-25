f87e994206098e340d4950c4516a39a921247d1c
Om Gupta <om.gupta@teknopoint.in>
2025-03-25 19:56:39 +0530

    DL added

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,2 +1,7 @@
 // add delayed functionality here
+let adobeScript = document.createElement("script");
+adobeScript.type = 'text/javascript';
+adobeScript.async = true;
+adobeScript.src = "https://assets.adobedtm.com/56086c44eca3/d7ef90251c90/launch-1aedac27ad88-development.min.js";
+document.body.appendChild(adobeScript);
 
