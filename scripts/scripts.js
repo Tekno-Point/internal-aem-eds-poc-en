dd4487258332dfc8ea4fed245d1b2c94a3ea9695
npy11 <Neeraj.yadav@teknopoint.local>
2025-03-25 19:24:42 +0530

    alalameyams nav added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -83,6 +83,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  wrapImgsInLinks(main)
 }
 
 /**
@@ -109,11 +110,35 @@ async function loadEager(doc) {
   }
 }
 
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
+function wrapImgsInLinks(container) {
+  const pictures = container.querySelectorAll('picture');
+  pictures.forEach((pic) => {
+    const link = pic.parentElement.nextElementSibling;
+    if (link?.classList.contains('button-container')) {
+      link.querySelector('a').innerHTML = '';
+      link.querySelector('a').append(pic)
+      // pic.replaceWith(link);
+    }
+  });
+}
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc);
   const main = doc.querySelector('main');
   await loadSections(main);
 
