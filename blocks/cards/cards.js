6ada0a9f27bccea5a107a6643bfaa72300123275
rahul.gohil1 <rahul.gohil@deptagency.com>
2025-03-26 13:15:47 +0530

    add css and style

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,11 +1,14 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import Swiper from '../swiper/swiper.min.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
+  const ul = document.createElement('div');
+  ul.classList.add("swiper-wrapper");
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+    const li = document.createElement('div');
+    li.classList.add("swiper-slide")
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
@@ -19,6 +22,68 @@ export default function decorate(block) {
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.textContent = '';
   block.append(ul);
+  console.log(block.classList.contains("alalameyams-swiper1"))
+if(block.classList.contains('alalameyams-swiper') || block.classList.contains('alalameyams-swiper1')){
+  const swiperPagination = document.createElement('div');
+  const swiperButtonPrev = document.createElement('div');
+  const swiperButtonNext = document.createElement('div');
+  swiperPagination.classList.add("swiper-pagination")
+  swiperButtonPrev.classList.add("swiper-button-next")
+  swiperButtonNext.classList.add("swiper-button-prev")
+
+
+  block.append(swiperPagination);
+  block.append(swiperButtonNext);
+  block.append(swiperButtonPrev);
+  block.append(swiperPagination);
+}
+new Swiper(".alalameyams-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    780: {
+      slidesPerView: 1,
+    }
+
+  },
+});
+  var swiper = new Swiper(".alalameyams-swiper1", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      780: {
+        slidesPerView: 4,
+      }
+
+    },
+  });
 }
