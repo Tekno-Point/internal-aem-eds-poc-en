17968d7703c4af838bf5dde89bb3bb2cc8e49ec5
Yogesh Walawalkar <yogesh.walwalkar@teknopoint.in>
2025-04-16 17:19:04 +0530

    Initial commit of frontend code

--- blocks/why-choose-us/why-choose-us.js
+++ blocks/why-choose-us/why-choose-us.js
@@ -1,43 +1,35 @@
-import {div} from "../../scripts/dom-helper";
-import Swiper from "../why-choose-us/swiper-bundle.min.js";
+import {
+  div
+} from "../../scripts/dom-helper.js";
+import SwiperInit from "../why-choose-us/swiper-bundle.min.js";
 
 export default function decorate(block) {
-  const swiper = div({class: 'swiper'});
-  const wrapper = div({class: 'swiper-wrapper'});
-  [...block.children].forEach((child) => {
-    child.classList.add('swiper-slide');
-    wrapper.appendChild(child);
-  });
-
-  swiper.appendChild(wrapper);
-  block.innerHTML = '';
-  block.appendChild(swiper);
-//   const pagination = document.createElement('div');
-//   pagination.className = 'swiper-pagination';
-//   swiper.appendChild(pagination);
-
-//   const nextBtn = document.createElement('div');
-//   nextBtn.className = 'swiper-button-next';
-//   const prevBtn = document.createElement('div');
-//   prevBtn.className = 'swiper-button-prev';
-//   swiper.appendChild(nextBtn);
-//   swiper.appendChild(prevBtn);
-
-  // Replace block content with swiper
-  block.innerHTML = '';
-  block.appendChild(swiper);
-  new Swiper(swiper, {
+  const swiper = div({
+      class: "swiper"
+    },
+    div({
+        class: "swiper-wrapper"
+      },
+      ...Array.from(block.children).map((child) => {
+        child.classList.add('swiper-slide');
+        return child;
+      })
+    )
+  )
+  block.textContent = '';
+  block.append(swiper)
+  SwiperInit(swiper, {
     slidesPerView: 4,
     spaceBetween: 20,
     loop: true,
-    pagination: {
-      el: pagination,
-      clickable: true,
-    },
-    navigation: {
-      nextEl: nextBtn,
-      prevEl: prevBtn,
-    },
+    // pagination: {
+    //   el: pagination,
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: nextBtn,
+    //   prevEl: prevBtn,
+    // },
   });
 
 }
