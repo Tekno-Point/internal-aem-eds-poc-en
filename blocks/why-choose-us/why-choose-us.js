55767b8b8963aee5a767cf28549223af2d0de4cf
Yogesh Walawalkar <yogesh.walwalkar@teknopoint.in>
2025-04-16 16:05:34 +0530

    carousel code added

---
+++ blocks/why-choose-us/why-choose-us.js
@@ -0,0 +1,50 @@
+import {
+  div
+} from "../../scripts/dom-helper";
+import Swiper from "../why-choose-us/swiper-bundle.min";
+
+export default function decorate(block) {
+  const swiper = div({
+    class: 'swiper'
+  });
+  const wrapper = div({
+    class: 'swiper-wrapper'
+  });
+
+  [...block.children].forEach((child) => {
+    child.classList.add('swiper-slide');
+    wrapper.appendChild(child);
+  });
+
+  swiper.appendChild(wrapper);
+  block.innerHTML = '';
+  block.appendChild(swiper);
+  const pagination = document.createElement('div');
+  pagination.className = 'swiper-pagination';
+  swiper.appendChild(pagination);
+
+  const nextBtn = document.createElement('div');
+  nextBtn.className = 'swiper-button-next';
+  const prevBtn = document.createElement('div');
+  prevBtn.className = 'swiper-button-prev';
+  swiper.appendChild(nextBtn);
+  swiper.appendChild(prevBtn);
+
+  // Replace block content with swiper
+  block.innerHTML = '';
+  block.appendChild(swiper);
+  new Swiper(swiper, {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: pagination,
+      clickable: true,
+    },
+    navigation: {
+      nextEl: nextBtn,
+      prevEl: prevBtn,
+    },
+  });
+
+}
