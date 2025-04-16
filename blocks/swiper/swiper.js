37c8237b8eaaa1dcb4624eabad4cc995d4c4899d
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-16 15:17:30 +0530

    added swiper

---
+++ blocks/swiper/swiper.js
@@ -0,0 +1,21 @@
+import Swiper from "../swiper/swiper-bundle.min.js"
+
+export default function decorate(block) {
+    console.log(block);
+    block.classList.add('swiper')
+    const Div = document.createElement('div')
+    Div.classList.add('swiper-wrapper')
+    Array.from(block.children).forEach((item) => {
+        item.classList.add('swiper-slide')
+        Div.append(item)
+    })
+    block.append(Div)
+    Swiper(block, {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 1000,
+        },
+    })
+
+}
