ed3d4a3114cf67e6902b779020da0d3ffbeb3665
Alok Singh <Alok.Singh@teknopoint.local>
2025-04-17 11:09:00 +0530

    swiper comp add

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -154,6 +154,7 @@ async function loadEager(doc) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
+  autolinkModals(doc)
   const main = doc.querySelector('main');
   await loadSections(main);
 
