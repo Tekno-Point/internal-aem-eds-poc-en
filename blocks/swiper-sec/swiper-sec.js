c95f7d0280df5d0fb6bd48961815fde9a96d4a37
Alok Singh <Alok.Singh@teknopoint.local>
2025-04-18 23:34:52 +0530

    accordion add

--- blocks/swiper-sec/swiper-sec.js
+++ blocks/swiper-sec/swiper-sec.js
@@ -5,43 +5,41 @@ import Swiper from '../testimonial/swiper-bundle.min.js';
 
 export default function decorate(block) {
     block.classList.add('swiper');
-
+  
     const swiperWrapper = document.createElement('div');
     const swiperPagination = document.createElement('div');
     swiperPagination.classList.add('swiper-pagination');
     swiperWrapper.classList.add('swiper-wrapper');
-
+  
     Array.from(block.children).forEach((row, index) => {
-        row.classList.add('swiper-slide');
-        
-        // Find <section> in the slide and modify its child <div>s
-        const section = row.querySelector('section');
-        if (section) {
-            const divs = section.querySelectorAll('div');
-            divs.forEach((div, i) => {
-                div.classList.add(`slide-${index + 1}-cards-${i + 1}`);
-            });
-        }
-
-        swiperWrapper.appendChild(row);
+      row.classList.add('swiper-slide');
+  
+      // Add unique class to each direct <div> inside .swiper-slide
+      const divs = row.querySelectorAll(':scope > div');
+      divs.forEach((div, divIndex) => {
+        div.classList.add(`swiper-slide-cards-${divIndex + 1}`);
+      });
+  
+      swiperWrapper.appendChild(row);
     });
-
+  
     block.appendChild(swiperWrapper);
     block.appendChild(swiperPagination);
-
+  
     Swiper(block, {
-        slidesPerView: "auto",
-        spaceBetween: 32,
-        slidesOffsetBefore: 0,
-        slidesOffsetAfter: 0,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: swiperPagination,
-            clickable: true,
-        },
+      slidesPerView: "auto",
+      spaceBetween: 32,
+      slidesOffsetBefore: 0,
+      slidesOffsetAfter: 0,
+    //   loop: true,
+    //   autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    //   },
+      pagination: {
+        el: swiperPagination,
+        clickable: true,
+      },
     });
-}
+  }
+  
