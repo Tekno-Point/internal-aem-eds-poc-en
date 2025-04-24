3b6bba3c713c15f2679a0fb8fb906fe41fa7fe9a
RupeshDept <rupesh.mahadik@deptagency.com>
2025-04-24 15:10:20 +0530

    added filter in defination

--- blocks/getintouch/getintouch.js
+++ blocks/getintouch/getintouch.js
@@ -3,13 +3,13 @@ import { div, input, button } from "../../scripts/dom-helpers.js";
 export default async function decorate(block) {
     block.textContent = '';
 
-    const formDiv = div(
-        { class: "form-maindiv" },
-        input({ class: "form-input firstname", placeholder: "First Name" }),
-        input({ class: "form-input lastName", placeholder: "last Name" }),
-        button({ class: "submit" }, "Submit")
-    );
-    block.append(formDiv)
+    // const formDiv = div(
+    //     { class: "form-maindiv" },
+    //     input({ class: "form-input firstname", placeholder: "First Name" }),
+    //     input({ class: "form-input lastName", placeholder: "last Name" }),
+    //     button({ class: "submit" }, "Submit")
+    // );
+    // block.append(formDiv)
     console.log("getintouch")
 
 }
