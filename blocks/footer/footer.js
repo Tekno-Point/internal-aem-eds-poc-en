0ed1f556e0d95ac1dd1ed7fc8d8119778d847f1c
karishmagosalia <karishma.gosalia@deptagency.com>
2025-05-19 17:19:34 +0530

    Footer completed

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -34,9 +34,44 @@ export default async function decorate(block) {
   if (columnsWrapper) {
     const columnsBlock = columnsWrapper.querySelector('.columns.block.columns-2-cols');
     if (columnsBlock) {
-      const colDivs = columnsBlock.querySelectorAll(':scope > div');
-      if (colDivs[0]) colDivs[0].classList.add('footer-col-left');
-      if (colDivs[1]) colDivs[1].classList.add('footer-col-right');
+      const colDivs = columnsBlock.querySelectorAll(':scope > div > div');
+      if (colDivs[0]) {
+        colDivs[0].classList.add('footer-col-left');
+        const leftPs = colDivs[0].querySelectorAll('p');
+        if (leftPs[0]) leftPs[0].classList.add('p1');
+        if (leftPs[1]) leftPs[1].classList.add('p2');
+      }
+      if (colDivs[1]) {
+        colDivs[1].classList.add('footer-col-right');
+        // Add class names to li elements inside the first ul in footer-col-right
+        const rightUl = colDivs[1].querySelector('ul');
+        if (rightUl) {
+          const rightLis = rightUl.querySelectorAll(':scope > li');
+          // Add class names to nested li elements in li1 (copyright, privacy, acknowledge)
+          if (rightLis[0]) {
+            rightLis[0].classList.add('li1');
+            const nestedUl1 = rightLis[0].querySelector('ul');
+            if (nestedUl1) {
+              const nestedLis1 = nestedUl1.querySelectorAll(':scope > li');
+              if (nestedLis1[0]) nestedLis1[0].classList.add('footer-copyright-li');
+              if (nestedLis1[1]) nestedLis1[1].classList.add('footer-privacy-li');
+              if (nestedLis1[2]) nestedLis1[2].classList.add('footer-acknowledge-li');
+            }
+          }
+          // Add class names to nested li elements in li2 (social icons)
+          if (rightLis[1]) {
+            rightLis[1].classList.add('li2');
+            const nestedUl2 = rightLis[1].querySelector('ul');
+            if (nestedUl2) {
+              const nestedLis2 = nestedUl2.querySelectorAll(':scope > li');
+              if (nestedLis2[0]) nestedLis2[0].classList.add('facebook');
+              if (nestedLis2[1]) nestedLis2[1].classList.add('instagram');
+              if (nestedLis2[2]) nestedLis2[2].classList.add('youtube');
+              if (nestedLis2[3]) nestedLis2[3].classList.add('linkedin');
+            }
+          }
+        }
+      }
     }
   }
 }
