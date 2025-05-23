7aed2671fc126eda70ebbaf1d1f0f65a5aa170ae
npy11 <Neeraj.yadav@teknopoint.local>
2025-05-23 16:18:52 +0530

    image url sheet path added

--- blocks/heromodelview/heromodelview.js
+++ blocks/heromodelview/heromodelview.js
@@ -1,3 +1,8 @@
-export default function decorate(block){
-    block
+export default function decorate(block) {
+
+   let props = Array.from(block.children).map((div) => div)
+   console.log(props)
+
+   block.innerHTML = '';
+   
 }
