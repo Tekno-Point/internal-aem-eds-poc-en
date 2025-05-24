e8dcf526e12d4eb5377ca52bd774c246d331741e
npy11 <Neeraj.yadav@teknopoint.local>
2025-05-24 22:28:43 +0530

    model js added

--- blocks/heromodelview/heromodelview.js
+++ blocks/heromodelview/heromodelview.js
@@ -1,24 +1,22 @@
 export default async function decorate(block) {
-    let selectedColor = 'white';
-    let clickedColor = 'white';
     let props = Array.from(block.children).map((div) => div)
     let imagesDetails = await renderDataFromAPI("/vida-v2-pro/model-images.json");
     const arrayImagesDet = imagesDetails.data;
-    selectedColor = arrayImagesDet[0].color;
+    let imgRotateUrls = arrayImagesDet.map((data) => data.img_rotate_urls)
     let modelWrapper = document.createElement("div");
-    modelWrapper.classList.add("model-wrapper")
+    modelWrapper.classList.add("model-wrapper");
 
-    let colors = document.createElement("div")
-    colors.classList.add("colors")
+    let colors = document.createElement("div");
+    colors.classList.add("colors");
 
     const selectedScooterClr = (div, ind) => {
-        // alert(ind)
-        div.classList.add("active")
-        modelWrapper.querySelector(".image").src = arrayImagesDet[ind].img_url;
+        div.classList.add("active");
+        let selImage = modelWrapper.querySelector(".image");
+        selImage.src = arrayImagesDet[ind].img_url;
     }
 
     arrayImagesDet.map((modelObj, ind) => {
-        let div = document.createElement("div")
+        let div = document.createElement("div");
         div.style.backgroundColor = modelObj.color;
         ind == 0 ? (div.classList.add("active", "color")) : (div.classList.add("color"));
         colors.appendChild(div);
@@ -30,38 +28,87 @@ export default async function decorate(block) {
         src='${arrayImagesDet[0].img_url}'>
     <div class="platform"></div>    
        <div class="colors">
-        ${colors.innerHTML.trim()}
+        ${colors.innerHTML}
         </div>
     <div class="visualizer-color-text-wrapper">
         <div class="color-text">6 colours to choose from</div>
     </div>
 </div>
-    `
-    console.log(modelWrapper);
-    // arrayImagesDet.forEach(element => {
-    //     if (clickedColor == selectedColor) {
-    //         scooterImg.src = element.img_url;
-    //     }
+    `;
 
-    // });
-    // div.append(img)
     block.innerHTML = '';
+
     let bgimgURL = props[0].querySelector("picture img").src;
     modelWrapper.style.backgroundImage = `url(${bgimgURL})`;
     let colorDiv = modelWrapper.querySelectorAll(".color");
-    colorDiv.forEach((div, ind) => {
+    let activeIndex = 0;
 
+    colorDiv.forEach((div, ind) => {
         div.addEventListener("click", () => {
-            colorDiv.forEach((eachDiv) => {
-                if (eachDiv.classList.contains("active")) {
-                    eachDiv.classList.remove("active")
-                }
-            })
+            changeActiveClr(colorDiv)
             selectedScooterClr(div, ind)
+            activeIndex = ind;
         });
 
-    })
-    block.appendChild(modelWrapper)
+    });
+    // let arrayImages = arrayImagesDet[activeIndex].img_rotate_urls.split(",")
+    const mainImage = modelWrapper.querySelector(".image");
+    mainImage.addEventListener("mousedown", (e) => {
+        rotateImg(e, activeIndex, arrayImagesDet,mainImage);
+    });
+    block.appendChild(modelWrapper);
+}
+
+let isDragging = false;
+let startX = 0;
+let currentFrame = 0;
+let accumulated = 0;
+
+const pixelsPerDegree = 1.5; // Adjust to control sensitivity
+
+const rotateImg = (event, activeIndex, arrayImagesDet, imgEl) => {
+    isDragging = true;
+    startX = event.clientX;
+
+    const imgRotateUrls = arrayImagesDet[activeIndex].img_rotate_urls.split(",");
+    const totalFrames = imgRotateUrls.length;
+    const degreesPerFrame = 360 / totalFrames;
+    const pixelsPerFrame = degreesPerFrame * pixelsPerDegree;
+
+    const onMouseMove = (e) => {
+        if (!isDragging) return;
+
+        const deltaX = e.clientX - startX;
+        accumulated += deltaX;
+        startX = e.clientX;
+
+        const frameShift = Math.floor(accumulated / pixelsPerFrame);
+        if (frameShift !== 0) {
+            accumulated -= frameShift * pixelsPerFrame;
+            currentFrame = (currentFrame + frameShift + totalFrames) % totalFrames;
+
+            imgEl.src = imgRotateUrls[currentFrame];
+        }
+    };
+
+    const onMouseUp = () => {
+        isDragging = false;
+        window.removeEventListener('mousemove', onMouseMove);
+        window.removeEventListener('mouseup', onMouseUp);
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
+    window.addEventListener('mouseup', onMouseUp);
+};
+
+
+
+const changeActiveClr = (colorDiv) => {
+    colorDiv.forEach((eachDiv) => {
+        if (eachDiv.classList.contains("active")) {
+            eachDiv.classList.remove("active")
+        }
+    });
 }
 
 async function renderDataFromAPI(url) {
