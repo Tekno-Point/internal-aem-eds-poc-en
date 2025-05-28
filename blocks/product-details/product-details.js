833e26856d4df6d59522f37689542d20eb0bc433
pashtepooja18 <poojapashte18@gmail.com>
2025-05-28 18:38:50 +0530

    class added to section

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -1,41 +1,35 @@
-// export default function decorate(block) {
-//     const configRow = block.children[0];
-//     if (!configRow) return;
+export default function decorate(block) {
+    const configRow = block.children[0];
+    if (!configRow) return;
 
-//     const columns = configRow.children[0]?.textContent.trim() || '5';
+    const columns = configRow.children[0]?.textContent.trim() || '5';
+    configRow.remove();
 
-//     // Remove the configuration row
-//     configRow.remove();
+    const productsGrid = document.createElement('div');
+    productsGrid.className = 'products-grid';
+    productsGrid.setAttribute('data-columns', columns);
 
-//     // Create the features grid
-//     const productsGrid = document.createElement('div');
-//     productsGrid.className = 'products-grid';
-//     productsGrid.setAttribute('data-columns', columns);
+    Array.from(block.children).forEach(productRow => {
+        const productIcon = productRow.children[0].children[0].children[0].children[0]|| '';
+        const productTitle = productRow.children[1]?.textContent.trim() || '';
+        const productValue = productRow.children[2]?.textContent.trim() || '';
+        const productUnit = productRow.children[3]?.textContent.trim() || '';
 
-//     Array.from(block.children).slice(2).forEach(productRow => {
-//         // console.log(productRow)
+        const productItem = document.createElement('div');
+        productItem.className = 'product-item';
 
-//         const productIcon = productRow.children[0]?.textContent.trim() || '';
-//         console.log(productIcon)
-//         const productTitle = productRow.children[1]?.textContent.trim() || '';
-//         const productValue = productRow.children[2]?.textContent.trim() || '';
-//         const productUnit = productRow.children[3]?.textContent.trim() || '';
-
-//         const productItem = document.createElement('div');
-//         productItem.className = 'product-item';
-
-//         productItem.innerHTML = `
-//            <img class="product-icon">${productIcon}</img>
-//            <div class="product-content">
-//                <h3 class="product-title">${productTitle}</h3>
-//                <div class="value-container">
-//                    <span class="product-value">${productValue}</span>
-//                    <span class="product-unit">${productUnit}<span>
-//                </div>
-//            </div>
-//         `
-//         productsGrid.appendChild(productItem)
-//     })
-//     block.innerHTML = '';
-//     block.appendChild(productsGrid)
-// }
+        productItem.innerHTML = `
+           <img class="product-icon">${productIcon.outerHTML}</img>
+           <div class="product-content">
+               <h3 class="product-title">${productTitle}</h3>
+               <div class="value-container">
+                   <span class="product-value">${productValue}</span>
+                   <span class="product-unit">${productUnit}<span>
+               </div>
+           </div>
+        `
+        productsGrid.appendChild(productItem)
+    })
+    block.innerHTML = '';
+    block.appendChild(productsGrid)
+}
