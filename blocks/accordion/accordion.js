cf0b0fb811069af873157878dd7a734dcff21676
pashtepooja18 <poojapashte18@gmail.com>
2025-05-29 23:18:12 +0530

    accordion component added

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,16 +1,26 @@
-// export default function decorate(block) {
-//     [...block.children].forEach((row) => {
-//       const label = row.children[0];
-//       const summary = document.createElement('summary');
-//       summary.className = 'accordion-item-label';
-//       summary.append(...label.childNodes);
+export default function decorate(block) {
+    [...block.children].forEach((row) => {
+      console.log(row)
+      const wrapper = document.createElement('div');
+      wrapper.className = 'accordion-wrapper';
 
-//       const body = row.children[1];
-//       body.className = 'accordion-item-body';
+      const label = row.children[0];
+      const summary = document.createElement('summary');
+      summary.className = 'accordion-item-label';
+      summary.append(...label.childNodes);
 
-//       const details = document.createElement('details');
-//       details.className = 'accordion-item';
-//       details.append(summary, body);
-//       row.replaceWith(details);
-//     });
-//   }
+      const body = row.children[1];
+      body.className = 'accordion-item-body';
+
+      const button = row.children[2];
+      button.className = 'know-more-button';
+
+      const details = document.createElement('details');
+      details.className = 'accordion-item';
+      details.append(summary, body);
+
+      wrapper.append(details);
+      wrapper.append(button);
+      row.replaceWith(wrapper);
+    });
+  } 
