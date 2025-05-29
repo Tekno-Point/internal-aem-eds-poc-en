9ca2935b3c4e0707260f3870bd44719368d30b4e
pashtepooja18 <poojapashte18@gmail.com>
2025-05-29 23:37:37 +0530

    accordion component added

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,26 +1,26 @@
-export default function decorate(block) {
-    [...block.children].forEach((row) => {
-      console.log(row)
-      const wrapper = document.createElement('div');
-      wrapper.className = 'accordion-wrapper';
+// export default function decorate(block) {
+//     [...block.children].forEach((row) => {
+//       console.log(row)
+//       const wrapper = document.createElement('div');
+//       wrapper.className = 'accordion-container';
 
-      const label = row.children[0];
-      const summary = document.createElement('summary');
-      summary.className = 'accordion-item-label';
-      summary.append(...label.childNodes);
+//       const label = row.children[0];
+//       const summary = document.createElement('summary');
+//       summary.className = 'accordion-item-label';
+//       summary.append(...label.childNodes);
 
-      const body = row.children[1];
-      body.className = 'accordion-item-body';
+//       const body = row.children[1];
+//       body.className = 'accordion-item-body';
 
-      const button = row.children[2];
-      button.className = 'know-more-button';
+//       const button = row.children[2];
+//       button.className = 'know-more-button';
 
-      const details = document.createElement('details');
-      details.className = 'accordion-item';
-      details.append(summary, body);
+//       const details = document.createElement('details');
+//       details.className = 'accordion-item';
+//       details.append(summary, body);
 
-      wrapper.append(details);
-      wrapper.append(button);
-      row.replaceWith(wrapper);
-    });
-  } 
+//       wrapper.append(details);
+//       wrapper.append(button);
+//       row.replaceWith(wrapper);
+//     });
+//   } 
