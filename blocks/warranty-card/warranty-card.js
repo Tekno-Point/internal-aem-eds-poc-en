7e1f0f57222ffcbb0a5ff64d8d42e03871af4962
pashtepooja18 <poojapashte18@gmail.com>
2025-06-01 22:52:40 +0530

    header, footer and component level css added

---
+++ blocks/warranty-card/warranty-card.js
@@ -0,0 +1,8 @@
+// export default function decorate(block) {
+//     const anchor = block.querySelector('a');
+//     anchor.innerHTML = "";
+//     const text = block.querySelector('p');
+//     console.log(text)
+
+//     // anchor.append(text);
+// }
