d6f2a19fce82b5b5b6b6babdfeda04d173a09a61
pashtepooja18 <poojapashte18@gmail.com>
2025-06-02 00:06:03 +0530

    header, footer and component level css added

--- blocks/warranty-card/warranty-card.js
+++ blocks/warranty-card/warranty-card.js
@@ -1,8 +1,14 @@
-// export default function decorate(block) {
-//     const anchor = block.querySelector('a');
-//     anchor.innerHTML = "";
-//     const text = block.querySelector('p');
-//     console.log(text)
+export default function decorate(block) {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'warranty-card-wrapper';
 
-//     // anchor.append(text);
-// }
+    const anchor = block.querySelector('a');
+    anchor.innerHTML = "";
+
+    const textPara = document.querySelector('.warranty-card > div:nth-child(2) p');
+    
+    block.innerHTML = "";
+    anchor.append(textPara);
+    wrapper.append(anchor);
+    block.append(wrapper)
+}
