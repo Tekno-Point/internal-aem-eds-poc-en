f252cbc426553d09799398ab7e01624f693641c1
pashtepooja18 <poojapashte18@gmail.com>
2025-06-02 09:44:37 +0530

    warranty card component added

--- blocks/warranty-card/warranty-card.js
+++ blocks/warranty-card/warranty-card.js
@@ -1,14 +1,22 @@
 export default function decorate(block) {
+    const title = block.children[0];
+    title.classList.add('warranty-card-title');
+
+    const images = block.children[1];
+    images.classList.add('warranty-card-images');
+  
     const wrapper = document.createElement('div');
-    wrapper.className = 'warranty-card-wrapper';
-
-    const anchor = block.querySelector('a');
-    anchor.innerHTML = "";
-
-    const textPara = document.querySelector('.warranty-card > div:nth-child(2) p');
-    
-    block.innerHTML = "";
-    anchor.append(textPara);
-    wrapper.append(anchor);
-    block.append(wrapper)
-}
+    wrapper.className = 'warranty-card';
+  
+    const link = document.createElement('a');
+    link.href = '#';
+    link.className = 'warranty-card-link'; 
+  
+    if (title) link.appendChild(title);
+    if (images) link.appendChild(images);
+  
+    wrapper.appendChild(link);
+  
+    block.innerHTML = '';
+    block.appendChild(wrapper);
+  }
