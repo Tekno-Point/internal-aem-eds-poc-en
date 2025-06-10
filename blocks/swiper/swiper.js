53237eeef3fa19690ac1070a9bbe0d75ec90a5a9
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-06-10 18:59:55 +0530

    styling in swiper

---
+++ blocks/swiper/swiper.js
@@ -0,0 +1,53 @@
+import SwiperText from "../swiper/swiper.min.js"
+export default function decorate(block) {
+  const paginationTexts = ['Brakes', 'i3S Technology', 'Rear Suspension', 'Seat','Side Stand Indicator','xSENS FI Technology','Tyer','Warranty'];
+  console.log(block);
+  block.classList.add("swiper");
+  const swapperWapper = document.createElement("div");
+  swapperWapper.classList.add("swiper-wrapper")
+  Array.from(block.children).forEach((element) => {
+    element.classList.add("swiper-slide")
+    swapperWapper.append(element);
+  })
+  block.append(swapperWapper)
+  // swiper-pagination
+  
+
+  const btnWrapper = document.createElement("div");
+  btnWrapper.classList.add("btnWrapper");
+
+  const divPagination = document.createElement("div");
+  divPagination.classList.add("swiper-pagination");
+  btnWrapper.append(divPagination);
+
+  const LeftArrow = document.createElement("div");
+  LeftArrow.classList.add("swiper-button-prev");
+  btnWrapper.appendChild(LeftArrow);
+
+  const RightArrow = document.createElement("div")
+  RightArrow.classList.add("swiper-button-next");
+  btnWrapper.appendChild(RightArrow);
+
+  block.append(btnWrapper)
+  SwiperText(block, {
+    loop:true,
+    navigation: {
+      nextEl: RightArrow,
+      prevEl: LeftArrow,
+    },
+    pagination: {
+      el: divPagination,
+      clickable: true,
+      renderBullet: function (index, className) {
+        // Use your text for each bullet based on index
+        return '<span class="' + className + '">' + paginationTexts[index] + '</span>';
+      }
+    },
+    freeMode: true,           // free scrolling mode
+    scrollbar: {
+      el: divPagination,
+      draggable: true,
+    },
+    scrollOnFocus : true
+  })
+}
