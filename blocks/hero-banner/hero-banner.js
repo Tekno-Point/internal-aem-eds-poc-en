6f6b006b405ecb0ee79c8e0008b405e3933c06d3
niharshah <nihar.shah@deptagency.com>
2025-06-11 14:24:08 +0530

    hero-banner update

---
+++ blocks/hero-banner/hero-banner.js
@@ -0,0 +1,8 @@
+export default function decorate(block) {
+    Array.from(block.children).forEach((elem) => {
+        elem.classList.add("inner")
+        Array.from(elem.children).forEach((elemSub, index) => {
+            elemSub.classList.add("innerSub" + (index + 1))
+        })
+    })
+}
