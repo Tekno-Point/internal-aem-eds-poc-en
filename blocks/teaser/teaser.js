c650feebdd183d6ecc634dcd0fbf50c1602f4bf4
pashtepooja18 <poojapashte18@gmail.com>
2025-06-12 11:36:57 +0530

    classes added to teaser

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,19 +1,19 @@
-export default function decorate(block) {
-    let childrens = block.children;
+// export default function decorate(block) {
+//     let childrens = block.children;
 
-    Array.from(childrens).forEach(elem => {
-        let firstElem = elem.children[0];
-        firstElem.classList.add("teaser-image");
-        let SecondElem = elem.children[1];
-        SecondElem.classList.add("teaser-content");
+//     Array.from(childrens).forEach(elem => {
+//         let firstElem = elem.children[0];
+//         firstElem.classList.add("teaser-image");
+//         let SecondElem = elem.children[1];
+//         SecondElem.classList.add("teaser-content");
 
-        let anchor = elem.querySelector('a');
-        let anchorLink = anchor.href;
+//         let anchor = elem.querySelector('a');
+//         let anchorLink = anchor.href;
         
-        let newAnchor = document.createElement('a');
-        newAnchor.href = anchorLink;
+//         let newAnchor = document.createElement('a');
+//         newAnchor.href = anchorLink;
 
-        newAnchor.append(elem);
-        block.append(newAnchor)
-    });
-}
+//         newAnchor.append(elem);
+//         block.append(newAnchor)
+//     });
+// }
