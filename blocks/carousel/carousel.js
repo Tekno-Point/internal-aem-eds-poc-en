b3f62b21812e723ad06c3ff9414f010ed3cd01d7
pashtepooja18 <poojapashte18@gmail.com>
2025-06-16 12:19:14 +0530

    hero carousel added

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,10 +1,29 @@
+import Swiper from '../carousel/swiper-bundle.min.js'
+
 export default function decorate(block) {
-    console.log(block)
     const rows = Array.from(block.children);
-    console.log(rows)
 
     const swiperWrapper = document.createElement('div');
+    swiperWrapper.classList.add('swiper-wrapper');
+
     const swiperPagination = document.createElement('div');
-    const swiperButtonPrev = document.createElement('div');
-    const swiperWrapper = document.createElement('div');
+    swiperPagination.classList.add('swiper-pagination');
+
+    rows.forEach((row) => {
+        row.classList.add('swiper-slide')
+        swiperWrapper.append(row);
+    })
+
+    block.append(swiperWrapper, swiperPagination);
+
+    swiperInit();
+}
+
+function swiperInit() {
+    new Swiper('.carousel.block', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable : true,
+        },
+    })
 }
