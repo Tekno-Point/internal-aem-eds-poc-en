460b1ffd5f0af6ffbf1a95258a7626fbe5453ee3
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-06-17 09:59:06 +0530

    update

--- blocks/buy-card/buy-card.js
+++ blocks/buy-card/buy-card.js
@@ -2,15 +2,12 @@ export default async function decorate(block) {
   const blockChildren = Array.from(block.children);
 
   blockChildren.forEach((child, index) => {
-    const className = `block-child-${index + 1}`;
+    const className = `block-child`+index;
     child.classList.add(className);
 
-    // If this is the first block child, add class to its children
-    if (index === 0) {
       const innerChildren = Array.from(child.children);
       innerChildren.forEach((innerChild,index) => {
         innerChild.classList.add('block-child1-child'+ index);
       });
-    }
   });
 }
