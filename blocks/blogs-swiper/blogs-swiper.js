5f1ca861d05cb7f038c78b268a3f5eaf43af906f
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-06-18 19:25:12 +0530

    update

--- blocks/blogs-swiper/blogs-swiper.js
+++ blocks/blogs-swiper/blogs-swiper.js
@@ -1,7 +1,72 @@
+// /*eslint-disable*/
+
+// import { div, h2, p, img, a } from "../../scripts/dom-helpers.js";
+
+// export default function decorate(block) {
+//   console.log("KKKKK");
+
+//   function getFakeApiData() {
+//     return [
+//       {
+//         id: 1,
+//         image: "https://picsum.photos/id/1015/250/150",
+//         title: "Mountain View",
+//         description: "A beautiful mountain view with mist and trees.",
+//         link: "#",
+//       },
+//       {
+//         id: 2,
+//         image: "https://picsum.photos/id/1016/250/150",
+//         title: "City Life",
+//         description: "The hustle and bustle of a vibrant cityscape.",
+//         link: "#",
+//       },
+//       {
+//         id: 3,
+//         image: "https://picsum.photos/id/1018/250/150",
+//         title: "Ocean Breeze",
+//         description: "Relaxing ocean waves on a sunny day.",
+//         link: "#",
+//       },
+//       {
+//         id: 4,
+//         image: "https://picsum.photos/id/1020/250/150",
+//         title: "Forest Trail",
+//         description: "A serene walk through the woods.",
+//         link: "#",
+//       },
+//     ];
+//   }
+
+//   const apiData = getFakeApiData();
+//   console.log(apiData);
+
+// const mainDiv = div(
+//   { class: "cards-container swiper" },
+//   div(
+//     { class: "swiper-wrapper" },
+//     ...apiData.map((data) =>
+//       div(
+//         { class: "swiper-slide" },
+//         img({ class: "my-image", src: data.image, alt: data.title }),
+//         h2({ class: "my-h2" }, data.title),
+//         p({ class: "my-p" }, data.description),
+//         a({ class: "my-a", href: data.link, target: "_blank" }, "View Image")
+//       )
+//     )
+//   )
+// );
+// block.appendChild(mainDiv);
+
+// }
+
 /*eslint-disable*/
-import { div, h2, p, img, a } from "../../scripts/domhelper.js";
+import BlocksSwiper from "../swiper/swiper.js"
+import { div, h2, p, img, a } from "../../scripts/dom-helpers.js";
 
 export default function decorate(block) {
+  console.log("KKKKK");
+
   function getFakeApiData() {
     return [
       {
@@ -36,61 +101,33 @@ export default function decorate(block) {
   }
 
   const apiData = getFakeApiData();
+  console.log(apiData);
 
-  const retirementCalculator = div(
-    { class: "main-Container" },
-    apiData.forEach((data)=>{
- div(
-      { class: "card" },
-      img("img src"),
-      h2("Title"),
-      p("para"),
-      a("#")
+  const mainDiv = div(
+    { class: "cards-container swiper" },
+    div(
+      { class: "swiper-wrapper" },
+      ...apiData.map((data) =>
+        div(
+          { class: "swiper-slide" },
+          div(
+            { class: "images" },
+            img({ class: "my-image", src: data.image, alt: data.title })
+          ),
+          div(
+            { class: "card-body" },
+            h2({ class: "my-h2" }, data.title),
+            p({ class: "my-p" }, data.description),
+            a(
+              { class: "my-a", href: data.link, target: "_blank" },
+              "View Image"
+            )
+          )
+        )
+      )
     )
-    })
   );
+  block.appendChild(mainDiv);
 
-  //   // Render cards dynamically
-  //   function renderCards(data) {
-  //     const container = document.getElementById("cardContainer");
-  //     container.innerHTML = "";
-  //     data.forEach((card) => {
-  //       const cards = document.createElement("div");
-  //       cards.className = "card";
-  //       cards.innerHTML = `
-  //           <img src="${card.image}" alt="${card.title}">
-  //           <h3>${card.title}</h3>
-  //           <p>${card.description}</p>
-  //           <a href="${card.link}">Read More</a>
-  //         `;
-  //       container.appendChild(div);
-  //     });
-  //   }
-  //   const apiData = getFakeApiData();
-  //   renderCards(apiData);
+  BlocksSwiper(block.children[0].nextSibling);
 }
-
-/* For Future: */
-/*   // Render cards dynamically
-  function renderCards(data) {
-    const container = document.getElementById("cardContainer");
-    container.innerHTML = ""; // Clear previous cards if any
-    data.forEach(card => {
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = `
-        <img src="${card.image}" alt="${card.title}">
-        <h3>${card.title}</h3>
-        <p>${card.description}</p>
-        <a href="${card.link}">Read More</a>
-      `;
-      container.appendChild(div);
-    });
-  }
-  // Fetch data from real API
-  fetch('https://your-api.com/cards')
-    .then(response => response.json())
-    .then(data => renderCards(data))
-    .catch(error => {
-      console.error("Error fetching data:", error);
-    }); */
