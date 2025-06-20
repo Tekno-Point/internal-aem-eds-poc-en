9dbc4c901b50298a18ccfb90151e7b9d5958432e
niharshah <nihar.shah@deptagency.com>
2025-06-20 20:13:50 +0530

    cf path issue fixed

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -3,7 +3,7 @@ export default async function decorate(block) {
     const url = `/graphql/execute.json/internal-aem-eds-poc/get-article;path=${formHref}`
     const response = await fetch((url), {
         method: "GET",
-        credentials: "include", // Include credentials such as cookies
+        // credentials: "include", // Include credentials such as cookies
     });
     const respData = await response.json();
     console.log("response :: ", respData);
