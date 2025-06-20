4e41f1309b5b4ca7fffddcf16f4799133c1033e3
niharshah <nihar.shah@deptagency.com>
2025-06-20 16:10:01 +0530

    form added

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,109 +1,178 @@
 import createField from './form-fields.js';
+import ffetch from '../../scripts/ffetch.js';
 
 async function createForm(formHref, submitHref) {
-  const { pathname } = new URL(formHref);
-  const resp = await fetch(pathname);
-  const json = await resp.json();
+    // console.log(formHref);
 
-  const form = document.createElement('form');
-  form.dataset.action = submitHref;
-  const formWrapper = document.createElement('div');
+    const url = formHref.split('/').filter(Boolean);
+    // console.log(url);
 
-  form.append(formWrapper);
-  const fields = await Promise.all(json.data.map((fd) => createField(fd, form)));
-  fields.forEach((field) => {
-    if (field.dataset.fieldset != 'submitfs') {
-      formWrapper.append(field);
-    }else{
-      form.append(field);
+    let fetchData, jsonData;
+
+    if (url.includes('graphql')) {
+        fetchData = async () => {
+            try {
+                const response = await fetch((formHref), {
+                    method: "GET",
+                    credentials: "include", // Include credentials such as cookies
+                });
+
+                const data = await response.json();
+
+                const capitalizedItems = data.data.configurationFrameworkWithoutMultifieldList.items.map(item => capitalizeKeys(item));
+                return capitalizedItems;
+
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        jsonData = await fetchData();
     }
-  });
+    else {
+        fetchData = await ffetch(formHref).all();
+        jsonData = fetchData;
+        //   console.log(jsonData)
+    }
 
-  // group fields into fieldsets
-  const fieldsets = form.querySelectorAll('fieldset');
-  fieldsets.forEach((fieldset) => {
-    form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
-      fieldset.append(field);
-      if(fieldset.name == 'submitfs'){
-        form.append(fieldset);
-      }
+    function capitalizeKeys(obj) {
+        if (Array.isArray(obj)) {
+            return obj.map(item => capitalizeKeys(item));
+        } else if (typeof obj === 'object' && obj !== null) {
+            return Object.keys(obj).reduce((result, key) => {
+                const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+                result[capitalizedKey] = capitalizeKeys(obj[key]);
+                return result;
+            }, {});
+        } else {
+            return obj;
+        }
+    }
+    // console.log(jsonData)
+
+    const form = document.createElement('form');
+    form.dataset.action = submitHref;
+
+    const submitFormBtn = document.createElement('button');
+    submitFormBtn.type = 'submit';
+    submitFormBtn.textContent = 'Submit';
+    submitFormBtn.classList.add('submit-btn');
+
+    const fields = await Promise.all(jsonData.map((fd) => createField(fd, form)));
+    // console.log(fields);
+    fields.forEach((field) => {
+        if (field) {
+            form.append(field);
+        }
     });
-  });
 
-  return form;
+    // group fields into fieldsets
+    const fieldsets = form.querySelectorAll('fieldset');
+    fieldsets.forEach((fieldset) => {
+        form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
+            fieldset.append(field);
+        });
+    });
+
+    form.appendChild(submitFormBtn);
+    return form;
 }
 
 function generatePayload(form) {
-  const payload = {};
+    // console.log(form);
+    const payload = {};
 
-  [...form.elements].forEach((field) => {
-    if (field.name && field.type !== 'submit' && !field.disabled) {
-      if (field.type === 'radio') {
-        if (field.checked) payload[field.name] = field.value;
-      } else if (field.type === 'checkbox') {
-        if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
-      } else {
-        payload[field.name] = field.value;
-      }
+    [...form.elements].forEach((field) => {
+        if (field.name && field.type !== 'submit' && !field.disabled) {
+            if (field.type === 'radio') {
+                if (field.checked) payload[field.name] = field.value;
+            } else if (field.type === 'checkbox') {
+                if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
+            } else {
+                payload[field.name] = field.value;
+            }
+        }
+    });
+    return payload;
+}
+
+function formSubmitData(form) {
+    let formattedData = '';
+    const formData = {};
+
+    [...form.elements].forEach((field) => {
+        formData[field.name] = field.value;
+    })
+
+    for (const [key, value] of Object.entries(formData)) {
+        formattedData += `${key} : ${value} \n`
     }
-  });
-  return payload;
+
+    alert(formattedData);
+    return formData;
 }
 
 async function handleSubmit(form) {
-  if (form.getAttribute('data-submitting') === 'true') return;
+    if (form.getAttribute('data-submitting') === 'true') return;
 
-  const submit = form.querySelector('button[type="submit"]');
-  try {
-    form.setAttribute('data-submitting', 'true');
-    submit.disabled = true;
+    const submit = form.querySelector('button[type="submit"]');
+    const formSubmit = document.querySelector('form');
 
-    // create payload
-    const payload = generatePayload(form);
-    const response = await fetch(form.dataset.action, {
-      method: 'POST',
-      body: JSON.stringify({ data: payload }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (response.ok) {
-      if (form.dataset.confirmation) {
-        window.location.href = form.dataset.confirmation;
-      }
-    } else {
-      const error = await response.text();
-      throw new Error(error);
+    formSubmit.addEventListener('submit', ()=> {
+        e.preventDefault();
+        formSubmitData(form);
+    })
+    try {
+        form.setAttribute('data-submitting', 'true');
+        submit.disabled = true;
+
+        // create payload
+        const payload = generatePayload(form);
+        const formData = formSubmitData(form);
+        const response = await fetch(form.dataset.action, {
+            method: 'POST',
+            body: JSON.stringify({ data: payload }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            form.reset();
+            if (form.dataset.confirmation) {
+                window.location.href = form.dataset.confirmation;
+            }
+        } else {
+            const error = await response.text();
+            throw new Error(error);
+        }
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+    } finally {
+        form.setAttribute('data-submitting', 'false');
+        submit.disabled = false;
     }
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-  } finally {
-    form.setAttribute('data-submitting', 'false');
-    submit.disabled = false;
-  }
 }
 
 export default async function decorate(block) {
-  const links = [...block.querySelectorAll('a')].map((a) => a.href);
-  const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
-  const submitLink = links.find((link) => link !== formLink);
-  if (!formLink || !submitLink) return;
+    const links = [...block.querySelectorAll('a')].map((a) => a.href);
+    const formLink = links.find((link) => link.includes('.json') || link.endsWith('.json'));
+    const submitLink = links.find((link) => link !== formLink);
+    if (!formLink || !submitLink) return;
 
-  const form = await createForm(formLink, submitLink);
-  block.replaceChildren(form);
+    const form = await createForm(formLink, submitLink);
+    block.replaceChildren(form);
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const valid = form.checkValidity();
-    if (valid) {
-      handleSubmit(form);
-    } else {
-      const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
-      if (firstInvalidEl) {
-        firstInvalidEl.focus();
-        firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
-      }
-    }
-  });
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const valid = form.checkValidity();
+        if (valid) {
+            handleSubmit(form);
+        } else {
+            const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
+            if (firstInvalidEl) {
+                firstInvalidEl.focus();
+                firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    });
 }
