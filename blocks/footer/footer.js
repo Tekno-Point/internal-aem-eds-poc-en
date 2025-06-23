01e14f7ad12585c2c43fa69ec848b222fdb91cdd
Alok Singh <Alok.Singh@teknopoint.local>
2025-06-23 21:00:14 +0530

    code add sd

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,36 +1,36 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-import accoridanBlock from "../accordion/accordion.js"
+// import { getMetadata } from '../../scripts/aem.js';
+// import { loadFragment } from '../fragment/fragment.js';
+// import accoridanBlock from "../accordion/accordion.js"
 
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
-export default async function decorate(block) {
+// /**
+//  * loads and decorates the footer
+//  * @param {Element} block The footer block element
+//  */
+// export default async function decorate(block) {
 
-  Array.from(block.children).forEach((row, rowIndex) => {
-    row.classList.add("footer-content");
-    row.classList.add(`sec-${rowIndex + 1}`);
-    Array.from(row.children).forEach((column, colIndex) => {
-      column.classList.add("footer-content-column");
-      column.classList.add(`sebtxt-${colIndex + 1}`);
-    });
-  });
-  const listBlocks = block.querySelectorAll(".footer-content-column ul");
-  listBlocks.forEach((ul, index) => {
-    ul.classList.add('footer-content-ul');
-  });
-  accoridanBlock(block)
-  // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
-  const fragment = await loadFragment(footerPath);
+//   Array.from(block.children).forEach((row, rowIndex) => {
+//     row.classList.add("footer-content");
+//     row.classList.add(`sec-${rowIndex + 1}`);
+//     Array.from(row.children).forEach((column, colIndex) => {
+//       column.classList.add("footer-content-column");
+//       column.classList.add(`sebtxt-${colIndex + 1}`);
+//     });
+//   });
+//   const listBlocks = block.querySelectorAll(".footer-content-column ul");
+//   listBlocks.forEach((ul, index) => {
+//     ul.classList.add('footer-content-ul');
+//   });
+//   // accoridanBlock(block)
+//   // load footer as fragment
+//   const footerMeta = getMetadata('footer');
+//   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+//   const fragment = await loadFragment(footerPath);
 
-  // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+//   // decorate footer DOM
+//   block.textContent = '';
+//   const footer = document.createElement('div');
+//   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   
-  block.append(footer);
-}
+//   block.append(footer);
+// }
