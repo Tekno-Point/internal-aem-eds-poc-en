602e8b4628a4719e3f3215372488f38578055e97
Alok Singh <Alok.Singh@teknopoint.local>
2025-06-23 15:47:58 +0530

    code add

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -7,6 +7,19 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
 
+  Array.from(block.children).forEach((row, rowIndex) => {
+    row.classList.add("footer-content");
+    row.classList.add(`sec-${rowIndex + 1}`);
+    Array.from(row.children).forEach((column, colIndex) => {
+      column.classList.add("footer-content-column");
+      column.classList.add(`sebtxt-${colIndex + 1}`);
+    });
+  });
+  const listBlocks = block.querySelectorAll(".footer-content-column ul");
+  listBlocks.forEach((ul, index) => {
+    ul.classList.add(`footer-content-ul-${index + 1}`);
+  });
+
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
@@ -18,13 +31,4 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
-
-  Array.from(block.children).forEach((row, rowIndex) => {
-    row.classList.add("find-us-container");
-    row.classList.add(`sec-${rowIndex + 1}`);
-    Array.from(row.children).forEach((column, colIndex) => {
-      column.classList.add("find-us-container-column");
-      column.classList.add(`sebtxt-${colIndex + 1}`);
-    });
-  });
 }
