ecdb943f2f786186a0188d4628a7ba51422e6430
Om Gupta <om.gupta@teknopoint.in>
2025-06-23 13:28:43 +0530

    CF imp done

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,10 +1,16 @@
 export default async function decorate(block) {
     const formHref = new URL(block.querySelector('a')?.href).pathname;
-    const url = `/graphql/execute.json/internal-aem-eds-poc/get-article;path=${formHref}`
+    const configResp = await fetch('/config.json');
+    const config = await configResp.json();
+    const url = `${config.data[0].value}/graphql/execute.json/internal-aem-eds-poc/get-article;path=${formHref}`
     const response = await fetch((url), {
         method: "GET",
         // credentials: "include", // Include credentials such as cookies
     });
     const respData = await response.json();
+    block.innerHTML = JSON.stringify(respData.data.articleByPath.item);
+    // respData.data.forEach(data => {
+        
+    // });
     console.log("response :: ", respData);
 }
