922c4eab50cd83d23addeb3ed243e4527c98ae54
pashtepooja18 <poojapashte18@gmail.com>
2025-06-24 13:24:27 +0530

    multi property added

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,16 +1,48 @@
 export default async function decorate(block) {
-    const formHref = new URL(block.querySelector('a')?.href).pathname;
+    // const formHref = new URL(block.querySelectorAll('a')?.href).pathname;
+    const formHref = block.querySelectorAll('a');
+
     const configResp = await fetch('/config.json');
     const config = await configResp.json();
-    const url = `${config.data[0].value}/graphql/execute.json/internal-aem-eds-poc/get-article;path=${formHref}`
-    const response = await fetch((url), {
-        method: "GET",
-        // credentials: "include", // Include credentials such as cookies
-    });
-    const respData = await response.json();
-    block.innerHTML = JSON.stringify(respData.data.articleByPath.item);
-    // respData.data.forEach(data => {
-        
-    // });
-    console.log("response :: ", respData);
+
+    block.innerHTML = "";
+
+    for (const item of formHref) { 
+        const formurl =  new URL(item.href).pathname
+        const url = `${config.data[0].value}/graphql/execute.json/internal-aem-eds-poc/get-article;path=${formurl}`
+        const response = await fetch((url), {
+            method: "GET",
+            // credentials: "include", // Include credentials such as cookies
+        });
+        const respData = await response.json();
+        // block.innerHTML = JSON.stringify(respData.data.articleByPath.item);
+        // respData.data.forEach(data => {
+            
+        // });
+        console.log("response :: ", respData);
+
+        createCard(respData, block)
+    }
+}
+
+
+function createCard (data, block) {
+    const path = data.data.articleByPath.item;
+    const teaser = document.createElement('div');
+    teaser.classList.add('teaser');
+
+    teaser.innerHTML = `
+        <a href=${path.ctaUrl._publishUrl}>
+            <div class="teaser-image">
+                <img loading="lazy" alt="" src=${path.image._publishUrl}/>
+            </div>
+            <div class="teaser-content">
+              <h4 id="basic-health-insurance">${path.title}</h4>
+              <p>${path.description}</p>
+              <p class="button-container">${path.ctaText}</p>
+            </div>
+       </a>
+    `
+    block.appendChild(teaser);
+    // console.log(teaser)
 }
