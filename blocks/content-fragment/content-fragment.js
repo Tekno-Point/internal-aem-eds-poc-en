ad7f42d3fd5da56307667ff52e50bf7590dce280
Om Gupta <om.gupta@teknopoint.in>
2025-06-24 19:06:26 +0530

    cf code handle

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -1,4 +1,9 @@
+const exclude = ['author-p48457-e1275402.adobeaemcloud.com'];
 export default async function decorate(block) {
+    
+    if(!exclude.includes(window.location.host)){
+        return block;
+    }
     // const formHref = new URL(block.querySelectorAll('a')?.href).pathname;
     const formHrefs = block.querySelectorAll('a');
 
