9478f849bb5a17b9bc4eee1e8c3d8395c9cc9f78
npy11 <Neeraj.yadav@teknopoint.local>
2025-06-25 11:40:31 +0530

    tab Component js added

--- blocks/tabproduct/tabproduct.js
+++ blocks/tabproduct/tabproduct.js
@@ -1,3 +1,27 @@
-export default function decorate(block){
-    
+export default function decorate(block) {
+    const itemObj = [];
+    let itemsDom = [...block.children].map((row, ind) => {
+        itemObj.push({ item: row.children });
+        return `
+            <div class="bgTab">
+	            <div class="progresLine">
+	            </div>
+	            <div id="personalTb">
+		            ${row?.children[1]?.textContent?.trim()}
+	            </div>
+	            <div class="d-none rightArrow mob-show">
+		            <img src="${row?.children[0]?.querySelector("img")?.src}">
+	            </div>
+            </div>
+        `
+    })
+    let div = document.createElement("div")
+
+    div.innerHTML = itemsDom.join("")
+
+    block.innerHTML = '';
+    console.log(itemsDom.join(""))
+    console.log(block);
+    block.append(div)
+
 }
