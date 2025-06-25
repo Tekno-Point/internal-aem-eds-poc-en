b74516cf226544d4b4c737ceb8caec481bad7bca
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-25 10:42:47 +0530

    Added Class via js

--- blocks/banner-carousel/banner-carousel.js
+++ blocks/banner-carousel/banner-carousel.js
@@ -1,4 +1,21 @@
 export default function decorate(block){
     console.log(block);
-    
+    Array.from(block.children).forEach((element,index)=>{
+        element.classList.add("bannerCarousel")
+        Array.from(element.children).forEach((elem,ind)=>{
+            if (ind !== 0) {
+                elem.classList.add("subBanner"+(index+1)+"part"+(ind+1))   
+            }else{
+                elem.classList.add("subBannerpart")
+            }
+        })
+    })
+    Array.from(block.querySelectorAll(".subBannerpart ul")).forEach((el,index)=>{
+        Array.from(el.children).forEach((elem)=>{
+            const divel = document.createElement("div");
+            divel.classList.add("roundrestat")
+            divel.append(elem)
+            block.querySelectorAll(".subBannerpart ul")[index].append(divel)
+        })
+    })
 }
