8b70bcb236788097c5e3394b22ab2826a0f06f44
npy11 <Neeraj.yadav@teknopoint.local>
2025-06-25 16:18:22 +0530

    home page css js added

--- blocks/tabproduct/tabproduct.js
+++ blocks/tabproduct/tabproduct.js
@@ -1,27 +1,43 @@
 export default function decorate(block) {
-    const itemObj = [];
+    const isMobile = window.matchMedia('(max-width: 767px)').matches;
+
+    const bgImg = [];
     let itemsDom = [...block.children].map((row, ind) => {
-        itemObj.push({ item: row.children });
+        let imgSrc = row?.children[0]?.querySelector("img")?.src;
+        bgImg.push({ imgSrc })
         return `
-            <div class="bgTab">
+            <div class="bgTab" id="${ind + 1}" ${isMobile ? `style="background-image: url('${row?.children[0]?.querySelector("img")?.src || ''}')"` : ''}>
 	            <div class="progresLine">
 	            </div>
-	            <div id="personalTb">
-		            ${row?.children[1]?.textContent?.trim()}
+	            <div class="tab">
+		            ${row?.children[1]?.textContent?.trim() || ''}
 	            </div>
-	            <div class="d-none rightArrow mob-show">
-		            <img src="${row?.children[0]?.querySelector("img")?.src}">
+                 <div class="d-none righ    tArrow mob-show">
+		            <img src="${row?.children[2]?.querySelector("img")?.src || ''}">
 	            </div>
             </div>
         `
     })
-    let div = document.createElement("div")
 
-    div.innerHTML = itemsDom.join("")
+    block.innerHTML = `
+        <div class="bgTabwrapper" ${isMobile ? '' : `style="background-image: url('${bgImg[0]?.imgSrc}')"`}>
+            <div class="bgTab-container">
+                ${itemsDom?.join("") || ''}
+            </div>
+        </div>
+    `;
 
-    block.innerHTML = '';
-    console.log(itemsDom.join(""))
-    console.log(block);
-    block.append(div)
+    let bgTabwrapper = block.querySelector(".bgTabwrapper");
+    const bgTabs = block.querySelectorAll(".bgTab");
+    if (!isMobile) {
+        bgTabs.forEach(tab => {
+            tab.addEventListener("mouseover", (e) => {
+                let cureentId = e.currentTarget.id;
+                bgTabwrapper.style.backgroundImage = `url('${bgImg[cureentId - 1]?.imgSrc || ''}')`
+                console.log(e.currentTarget)
+            })
+        });
+    }
+    // block.append(bgTabContainer)
 
 }
