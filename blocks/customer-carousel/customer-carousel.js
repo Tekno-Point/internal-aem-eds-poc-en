8eb6e0553614e5f9c36a462813367384aa700b53
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-26 12:01:13 +0530

    updateed

--- blocks/customer-carousel/customer-carousel.js
+++ blocks/customer-carousel/customer-carousel.js
@@ -1,4 +1,20 @@
 export default function decorate(block){
     console.log(block);
-    
+    Array.from(block.children).forEach((el)=>{
+        el.classList.add("custcarousel");
+        Array.from(el.children).forEach((elsub,ind)=>{
+            elsub.classList.add("custCaro"+(ind+1))
+        })
+    })
+    block.querySelectorAll(".custCaro2").forEach((elem)=>{
+        Array.from(elem.children).forEach((subel,index)=>{
+            if (index == 0) {
+                subel.classList.add("title")
+            }
+            if (index == 1) {
+                subel.classList.add("design")
+            }
+        })
+    })
+
 }
