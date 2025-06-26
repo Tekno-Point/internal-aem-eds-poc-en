f57e1bb2f4642a4bbc3661f024d572679c4da7ab
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-06-26 15:07:18 +0530

    added show more

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -81,4 +81,29 @@ export default async function decorate(block) {
   button.textContent = "View More";
   button.className = "view-more-btn";
   document.querySelector(".footer-sec3 div").appendChild(button);
+
+
+  document.querySelector('.view-more-btn').addEventListener('click' , function(){
+    console.log("inn")
+    document.querySelector('.footer-sec3 div').childNodes.forEach(function(item , index){
+        if(!item.classList.contains('sec3-ul-show')){
+            item.classList.add(`sec3-ul-show`)
+            item.classList.remove(`sec3-ul-hide`)
+        } else{
+            if(index < 2){
+                item.classList.add(`sec3-ul-show`)
+            }else{
+                item.classList.add(`sec3-ul-hide`)
+                item.classList.remove(`sec3-ul-show`)
+            }
+        }
+    })
+    document.querySelector('.view-more-btn').classList.remove(`sec3-ul-hide`)
+    if(document.querySelector('.view-more-btn').textContent == 'View More'){
+                document.querySelector('.view-more-btn').textContent = 'View Less'
+    }else if(document.querySelector('.view-more-btn').textContent == 'View Less'){
+             document.querySelector('.view-more-btn').textContent = 'View More'
+    }
+    console.log("out")
+})
 }
