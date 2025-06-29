8bdb2dc6813ee94c6169ce29026ccc75bb6f5c8d
npy11 <Neeraj.yadav@teknopoint.local>
2025-06-29 12:03:23 +0530

    tab block js modified

--- blocks/tabproduct/tabproduct.js
+++ blocks/tabproduct/tabproduct.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
 
-    // if (window.location.href.includes("author-p48457-e1275402.adobeaemcloud.com")) return block;
+    if (window.location.href.includes("author-p48457-e1275402.adobeaemcloud.com")) return block;
     const isMobile = window.matchMedia('(max-width: 767px)').matches;
 
     const bgImg = [];
