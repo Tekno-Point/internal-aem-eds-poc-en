00608b5baab6be33ab53dfb1f47c3fe1a5606287
AnuragPandey <anurag.pandey@teknopoint.in>
2025-06-30 11:23:51 +0530

    added

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,56 +1,51 @@
-import SwipperText from "../swiper/swiper-bundle.min.js"
+import SwipperText from "../swiper/swiper-bundle.min.js";
 export default function decorate(block) {
-
   block.classList.add("swiper");
   const swipperwrapper = document.createElement("div");
   swipperwrapper.classList.add("swiper-wrapper");
   Array.from(block.children).forEach((eleChild) => {
     eleChild.classList.add("swiper-slide");
     swipperwrapper.append(eleChild);
-  })
+  });
   block.append(swipperwrapper);
   let blockClassList = Array.from(block.classList);
 
-
-
-
   function handleResolution() {
     var objConfig = {};
-if (window.innerWidth < 768) {   
-    
+    if (window.innerWidth < 768) {
       //Mobile
       if (blockClassList.indexOf("one-point-five-view") != -1) {
-        objConfig['slidesPerView'] = 1.2;
-        objConfig['spaceBetween'] = 10;
+        objConfig["slidesPerView"] = 1.2;
+        objConfig["spaceBetween"] = 10;
       }
       if (blockClassList.indexOf("two-point-five-view") != -1) {
-        objConfig['slidesPerView'] = 2.5;
-        objConfig['spaceBetween'] = 20;
+        objConfig["slidesPerView"] = 2.5;
+        objConfig["spaceBetween"] = 20;
       }
       if (blockClassList.indexOf("three-point-five-view") != -1) {
-        objConfig['slidesPerView'] = 3.5;
-        objConfig['spaceBetween'] = 30;
+        objConfig["slidesPerView"] = 3.5;
+        objConfig["spaceBetween"] = 30;
       }
       if (blockClassList.indexOf("mobile-one-point-five-view") != -1) {
-        objConfig['slidesPerView'] = 1.5;
-        objConfig['spaceBetween'] = 10;
+        objConfig["slidesPerView"] = 1.5;
+        objConfig["spaceBetween"] = 10;
       }
       if (blockClassList.indexOf("center-view") != -1) {
-        objConfig['slidesPerView'] = 1.4;
-        objConfig['centeredSlides'] = true;
+        objConfig["slidesPerView"] = 1.4;
+        objConfig["centeredSlides"] = true;
         // objConfig['spaceBetween'] = 30;
       }
       if (blockClassList.indexOf("mobile-pagination") != -1) {
-        let paginationDots = document.createElement('div');
-        paginationDots.classList.add('swiper-pagination');
+        let paginationDots = document.createElement("div");
+        paginationDots.classList.add("swiper-pagination");
         block.append(paginationDots);
-        objConfig['pagination'] = {
+        objConfig["pagination"] = {
           el: paginationDots,
           clickable: true,
-        }
+        };
       }
       if (blockClassList.indexOf("mobile-loop") != -1) {
-        objConfig["loop"] = true
+        objConfig["loop"] = true;
       }
       if (blockClassList.indexOf("mobile-auto-scroll") != -1) {
         objConfig["loop"] = true;
@@ -58,46 +53,50 @@ if (window.innerWidth < 768) {
       }
     } else {
       //DeskTop
+      if (blockClassList.indexOf("mobile-auto-scroll") != -1) {
+        objConfig["loop"] = true;
+        objConfig["autoplay"] = true;
+      }
       if (blockClassList.indexOf("desk-pagination") != -1) {
-        let paginationDots = document.createElement('div');
-        paginationDots.classList.add('swiper-pagination');
+        let paginationDots = document.createElement("div");
+        paginationDots.classList.add("swiper-pagination");
         block.append(paginationDots);
-        objConfig['pagination'] = {
+        objConfig["pagination"] = {
           el: paginationDots,
           clickable: true,
-        }
+        };
       }
 
       if (blockClassList.indexOf("left-right-navigation-btn") != -1) {
-        let navNextbtn = document.createElement('div');
-        navNextbtn.classList.add('swiper-button-next');
+        let navNextbtn = document.createElement("div");
+        navNextbtn.classList.add("swiper-button-next");
         block.append(navNextbtn);
 
-        let navPrevbtn = document.createElement('div');
-        navPrevbtn.classList.add('swiper-button-prev');
+        let navPrevbtn = document.createElement("div");
+        navPrevbtn.classList.add("swiper-button-prev");
         block.append(navPrevbtn);
 
         objConfig["navigation"] = {
           nextEl: navNextbtn,
           prevEl: navPrevbtn,
-        }
+        };
       }
       if (blockClassList.indexOf("three-point-view") != -1) {
-        objConfig['slidesPerView'] = 3;
-        objConfig['spaceBetween'] = 30;
+        objConfig["slidesPerView"] = 3;
+        objConfig["spaceBetween"] = 30;
       }
       if (blockClassList.indexOf("one-point-five-view") != -1) {
-        objConfig['slidesPerView'] = 1.5;
-        objConfig['spaceBetween'] = 10;
+        objConfig["slidesPerView"] = 1.5;
+        objConfig["spaceBetween"] = 10;
       }
       if (blockClassList.indexOf("auto-scroll") != -1) {
-        objConfig["loop"] = true
-        objConfig["autoplay"] = true
-        objConfig['slidesPerView'] = 3.2;
-        objConfig['spaceBetween'] = 10;
+        objConfig["loop"] = true;
+        objConfig["autoplay"] = true;
+        objConfig["slidesPerView"] = 3.2;
+        objConfig["spaceBetween"] = 10;
       }
       if (blockClassList.indexOf("loop") != -1) {
-        objConfig["loop"] = true
+        objConfig["loop"] = true;
       }
     }
     SwipperText(block, objConfig);
@@ -108,7 +107,5 @@ if (window.innerWidth < 768) {
   handleResolution();
 
   // Also call it on resize
-  window.addEventListener('resize', handleResolution)
-
-
+  window.addEventListener("resize", handleResolution);
 }
