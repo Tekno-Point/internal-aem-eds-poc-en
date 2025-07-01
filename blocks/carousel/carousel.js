88af653afb9f8c0e9f699c8d9472fc2e11e2eb97
pashtepooja18 <poojapashte18@gmail.com>
2025-07-01 17:18:23 +0530

    carousel css added

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -17,7 +17,8 @@ export default function decorate(block) {
 
 
 function swiperInit(block) {
-    const swiperConfig = {}
+    const swiperConfig = {
+    };
 
     if (block.classList.contains('pagination')) {
         const swiperPagination = document.createElement('div');
@@ -26,6 +27,33 @@ function swiperInit(block) {
         swiperConfig.pagination = {
             el: '.carousel.block.pagination .swiper-pagination',
             clickable: true,
+        }
+    }
+
+    if (block.classList.contains('card-carousel')) {
+        swiperConfig.breakpoints = {
+            320 : {slidesPerView : 1, slidesPerGroup : 1},
+            1024 : {slidesPerView : 2, slidesPerGroup : 2, spaceBetween : 15}
+        }
+    }
+
+    if (block.classList.contains('image-link-carousel')) {
+        const slides = block.querySelectorAll('.swiper-slide');
+        slides.forEach(slide => {
+            const anchor = slide.querySelectorAll('p.button-container a');
+            const link = anchor.href;
+            const newAnchor = document.createElement('a');
+            newAnchor.href = link;
+
+            const picture = slide.querySelector('picture');
+            const imgWrapper = picture.parentElement;
+            newAnchor.append(picture);
+            imgWrapper.append(newAnchor);
+        })
+
+        swiperConfig.breakpoints = {
+            320 : {slidesPerView : 1, slidesPerGroup : 1},
+            1024 : {slidesPerView : 4, slidesPerGroup : 4, spaceBetween : 30}
         }
     }
     new Swiper(block, swiperConfig)
