089d5af5457cd82989a49f5028fa4db7a247dd3a
Yash-Kudve <yash.kudve@deptagency.com>
2025-07-07 09:56:43 +0530

    get in touch and personal loan type component added

--- blocks/git-block/git-block.js
+++ blocks/git-block/git-block.js
@@ -1,10 +1,7 @@
 export default function decorate(block) {
-  const wrapperDivs = Array.from(block.children);
+  const items = Array.from(block.children);
 
-  wrapperDivs.forEach((outerDiv) => {
-    outerDiv.classList.add('git-lists');
-
-    const innerDiv = outerDiv.querySelector('div');
-    if (innerDiv) innerDiv.classList.add('git-list-item');
+  items.forEach((item) => {
+    item.classList.add('git-lists');
   });
 }
