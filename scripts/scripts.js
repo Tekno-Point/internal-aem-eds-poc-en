b76ca0d7ef2ced5b8d5e9c5d5a5c8033dafd5ff0
npy11 <Neeraj.yadav@teknopoint.local>
2025-07-08 20:23:56 +0530

    loadsccript added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -608,7 +608,6 @@ async function loadLazy(doc) {
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
   loadAutoBlock(doc);
-  loadScript("https://assets.adobedtm.com/68d5933462dd/0f45052f726e/launch-2190e0b9d934-development.min.js", { async: true });
 }
 
 //////
