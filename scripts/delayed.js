d15393a062d9782bb7fa557bb1de815b3220a1da
npy11 <Neeraj.yadav@teknopoint.local>
2025-07-08 20:30:15 +0530

    loadsccript added in delayed js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,4 +1,4 @@
-import { loadScript } from "./aem";
+import { loadScript } from "./aem.js";
 
 // add delayed functionality here
   loadScript("https://assets.adobedtm.com/68d5933462dd/0f45052f726e/launch-2190e0b9d934-development.min.js", { async: true });
