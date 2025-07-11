11546900b086d4897b182a2777565a40da3e14cf
pashtepooja18 <poojapashte18@gmail.com>
2025-07-11 19:48:36 +0530

    class added to section

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -19,3 +19,21 @@ import('./plugin.js');
       }
     }, 1000);
   })();
+
+
+  function postMessageSpartan(name, data) {
+    try {
+      if (sessionStorage.getItem('spartanEnabled')) {
+        var spartanEvent = {
+          name: name,
+          data: data
+        };
+        window.ReactNativeWebView.postMessage(JSON.stringify(spartanEvent));
+      }
+    } catch (error) {
+      console.error("An error occurred in postMessageSpartan function:", error);
+    }
+  }
+  
+  const data = { url: "https://srilankan-airlines-mob--internal-aem-eds-poc-en--tekno-point.aem.live/srilankan-airlines-app/" };
+  postMessageSpartan("share", data);
