2f99933e0e86815e912305dd3ab8042225fe60d0
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-07-28 19:26:59 +0530

    trip-form empty quotes instead of null values

--- blocks/form/form-submits.js
+++ blocks/form/form-submits.js
@@ -4,24 +4,24 @@ import { travelinsurancecontinueClick } from "./analytics-script.js";
 function onTravelDetailsSubmit(form) {
   const tripFormData = new FormData(document.getElementById("single-trip"))
   const travelFormData = new FormData(form);
-  const tripCountry = document.querySelector("#single-trip .search-country-input")?.value;
+  const tripCountry = document.querySelector("#single-trip .search-country-input")?.value || "";
 
   travelinsurancecontinueClick(
-    'single-trip',
-    'lead-id',
-    tripFormData.get("geography-location"),
+    "single-trip",
+    "lead-id",
+    tripFormData.get("geography-location") || "",
     tripCountry,
     tripFormData.get("start date"),
     tripFormData.get("return date"),
     form.dataset.travellersAgeGroup,
     getDuration(tripFormData.get("start date"), tripFormData.get("return date")),
-    travelFormData.get("productId") || "travelinsurance",
-    "crmId",
+    "travelinsurance",
+    "",//"crmId"
     travelFormData.get("email"),
     travelFormData.get("mobile-number"),
-    "topup_amount",
-    "selected_tenure",
-    "Consent"
+    "",//"topup_amount"
+    "",//"selected_tenure"
+    "",//"Consent"
   )
 }
 
@@ -32,19 +32,19 @@ const onMultiTripSubmit = (form) => {
   travelinsurancecontinueClick(
     'multi-trip',
     'lead-id',
-    "NA",
-    "NA",
+    "",
+    "",
     multiTripFormData.get("tripstartdate"),
-    "Na",
+    "",
     multiTripFormData.get("ageradio"),
     multiTripFormData.get("dayradio"),
     "travelinsurance",
     "crmId",
-    "Na",
-    "Na",
-    "topup_amount",
-    "selected_tenure",
-    "Consent"
+    "",
+    "",
+    "",
+    "",
+    ""
   )
 }
 
