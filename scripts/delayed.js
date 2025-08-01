d8ae914bf3db735dda5fad236dccc6b55424c017
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-08-01 15:54:02 +0530

    delay.js added

---
+++ scripts/delayed.js
@@ -0,0 +1,18 @@
+// add delayed functionality here
+
+const fontAwesomeLink = document.createElement('link');
+fontAwesomeLink.rel = 'stylesheet';
+fontAwesomeLink.href = 'https://www.esafbank.com/wp-content/themes/esaf/css/homepage/font-awesome.css';
+document.head.appendChild(fontAwesomeLink);
+
+const fontLink = document.createElement('link');
+fontLink.rel = 'preconnect';
+fontLink.href = 'https://fonts.googleapis.com';
+
+const fontLink2 = document.createElement('link');
+fontLink2.rel = 'preconnect';
+fontLink2.href = 'https://fonts.gstatic.com';
+
+const fontLink3 = document.createElement('link');
+fontLink3.rel = 'stylesheet';
+fontLink3.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';
