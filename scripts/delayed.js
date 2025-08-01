f484bb62d28885b9f712be95efc3e2ec1f8b4fbb
Abhay Singh <Abhay.Singh@teknopoint.local>
2025-08-01 17:41:09 +0530

    fix header

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,18 +1,24 @@
 // add delayed functionality here
+const HEAD = document.head;
 
 const fontAwesomeLink = document.createElement('link');
 fontAwesomeLink.rel = 'stylesheet';
 fontAwesomeLink.href = 'https://www.esafbank.com/wp-content/themes/esaf/css/homepage/font-awesome.css';
-document.head.appendChild(fontAwesomeLink);
+HEAD.appendChild(fontAwesomeLink);
 
 const fontLink = document.createElement('link');
 fontLink.rel = 'preconnect';
 fontLink.href = 'https://fonts.googleapis.com';
+HEAD.appendChild(fontLink);
+
 
 const fontLink2 = document.createElement('link');
 fontLink2.rel = 'preconnect';
 fontLink2.href = 'https://fonts.gstatic.com';
+HEAD.appendChild(fontLink2);
+
 
 const fontLink3 = document.createElement('link');
 fontLink3.rel = 'stylesheet';
-fontLink3.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';
+fontLink3.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';4
+HEAD.appendChild(fontLink3);
