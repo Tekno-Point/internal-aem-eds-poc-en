2825fb509dcdc1a830ffd15a6d9a0ed3432c4297
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-08-01 14:31:33 +0530

    header-impl

---
+++ blocks/form/form.js
@@ -0,0 +1,172 @@
+import { ADD_TRAVELLERS } from './add-travellers.js';
+import callBackSubmit from './callback-submit.js';
+import createField from './form-fields.js';
+import { onCustomerDetailsSubmit, onLoanDetailsSubmit, onMultiTripSubmit, onOccupationDetailsSubmit, onTravelDetailsSubmit } from './form-submits.js';
+import formToggler, { getParentFromChildId } from './formToggler.js';
+import { SINGLE_TRIP } from './single-trip.js';
+
+async function createForm(formHref, submitHref) {
+  const { pathname } = new URL(formHref);
+  const resp = await fetch(pathname);
+  const json = await resp.json();
+
+  const form = document.createElement('form');
+  form.dataset.action = submitHref;
+
+  const fields = await Promise.all(json.data.map((fd) => createField(fd, form)));
+  fields.forEach((field) => {
+    if (field) {
+      form.append(field);
+    }
+  });
+
+  // group fields into fieldsets
+  const fieldsets = form.querySelectorAll('fieldset');
+  fieldsets.forEach((fieldset) => {
+    form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
+      fieldset.append(field);
+    });
+  });
+
+  return form;
+}
+
+function generatePayload(form) {
+  const payload = {};
+
+  [...form.elements].forEach((field) => {
+    if (field.name && field.type !== 'submit' && !field.disabled) {
+      if (field.type === 'radio') {
+        if (field.checked) payload[field.name] = field.value;
+      } else if (field.type === 'checkbox') {
+        if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
+      } else {
+        payload[field.name] = field.value;
+      }
+    }
+  });
+  return payload;
+}
+
+async function handleSubmit(form) {
+  if (form.getAttribute('data-submitting') === 'true') return;
+
+  const submit = form.querySelector('button[type="submit"]');
+  try {
+    form.setAttribute('data-submitting', 'true');
+    submit.disabled = true;
+
+    // create payload
+    const payload = generatePayload(form);
+    const response = await fetch(form.dataset.action, {
+      method: 'POST',
+      body: JSON.stringify({ data: payload }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      if (form.dataset.confirmation) {
+        window.location.href = form.dataset.confirmation;
+      }
+    } else {
+      const error = await response.text();
+      throw new Error(error);
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+  } finally {
+    form.setAttribute('data-submitting', 'false');
+    submit.disabled = false;
+  }
+}
+
+const formMatcher = (form, id) => {
+  switch (id) {
+    case 'single-trip':
+      formToggler('travel-details');
+      form.dataset.tripForm = JSON.stringify(new FormData(form));
+      ADD_TRAVELLERS(getParentFromChildId('travel-details', '.form-wrapper'));
+      break;
+    case 'multi-trip':
+      onMultiTripSubmit(form);
+      break;
+    case 'travel-details':
+      formToggler(id)
+      onTravelDetailsSubmit(form);
+      break;
+    case 'extend-policy':
+      formToggler(id)
+      break;
+    case 'customer-form':
+      onCustomerDetailsSubmit(form);
+      break;
+    case 'occupation-form':
+      onOccupationDetailsSubmit(form);
+      break;
+    case 'loan-form':
+      onLoanDetailsSubmit(form);
+      break;
+    case 'callback-submit':
+      callBackSubmit(form).then((res) => {
+        const result = JSON.parse(res.callbackSubmitResult);
+        if (result.success) {
+          document.body.classList.add('callback-submitted');
+          console.log("✅ Submission successful!");
+        } else {
+          console.error("❌ Submission failed:", result.errorMessage);
+        }
+      });
+      break;
+    default:
+      console.warn('no-mathching form');
+      const valid = form.checkValidity();
+      if (valid) {
+        handleSubmit(form);
+      } else {
+        const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
+        if (firstInvalidEl) {
+          firstInvalidEl.focus();
+          firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+  }
+}
+
+function onExtendPolicyClick(form) {
+  const extendPolicyButtons = form.querySelectorAll('#form-multi-trip-link-wrapper>div:nth-child(2) input, #form-single-trip-link-wrapper>div:nth-child(2) input');
+  extendPolicyButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      formToggler('extend-policy');
+    });
+  });
+}
+
+export default async function decorate(block) {
+  const links = [...block.querySelectorAll('a')].map((a) => a.href);
+  const formLink = links.find((link) => link.startsWith(window.location.origin) && link.endsWith('.json'));
+  const submitLink = links.find((link) => link !== formLink);
+  if (!formLink || !submitLink) return;
+
+  const formId = block.querySelector('div > div > p:nth-child(3)')?.textContent;
+
+  const form = await createForm(formLink, submitLink);
+
+  if (formId) form.id = formId;
+
+  block.replaceChildren(form);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formMatcher(form, formId);
+  });
+
+  onExtendPolicyClick(form);
+
+  SINGLE_TRIP(block)
+
+}
+
+
+export const decorateForm = decorate
