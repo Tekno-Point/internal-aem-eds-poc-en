0e133a5de68fe5f7ee68543680bc8da33e09603d
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-08-01 14:41:01 +0530

    fonst-commented

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,15 +5,15 @@ fontAwesomeLink.rel = 'stylesheet';
 fontAwesomeLink.href = 'https://www.esafbank.com/wp-content/themes/esaf/css/homepage/font-awesome.css';
 document.head.appendChild(fontAwesomeLink);
 
-const fontLink = document.createElement('link');
-fontLink.rel = 'preconnect';
-fontLink.href = 'https://fonts.googleapis.com';
+// const fontLink = document.createElement('link');
+// fontLink.rel = 'preconnect';
+// fontLink.href = 'https://fonts.googleapis.com';
 
-const fontLink2 = document.createElement('link');
-fontLink2.rel = 'preconnect';
-fontLink2.href = 'https://fonts.gstatic.com';
+// const fontLink2 = document.createElement('link');
+// fontLink2.rel = 'preconnect';
+// fontLink2.href = 'https://fonts.gstatic.com';
 
-const fontLink3 = document.createElement('link');
-fontLink3.rel = 'stylesheet';
-fontLink3.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';
+// const fontLink3 = document.createElement('link');
+// fontLink3.rel = 'stylesheet';
+// fontLink3.href = 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap';
 
