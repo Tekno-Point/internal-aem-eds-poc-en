80b64642309eaa864f2f7039a63ecdeb695e7e79
Om Gupta <om.gupta@teknopoint.in>
2025-08-05 12:13:22 +0530

    third party script added

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,5 @@
-// add delayed functionality here
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://assets.adobedtm.com/56086c44eca3/d7ef90251c90/launch-1aedac27ad88-development.min.js';
+script.async = true;
+document.body.appendChild(script);
