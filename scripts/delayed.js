d11cd27b88a598ee9738a9fea324e5be0eebe7dd
Om Gupta <om.gupta@teknopoint.in>
2025-08-05 12:39:28 +0530

    Load Third party script

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,8 @@
-// add delayed functionality here
+import { fetchPlaceholders } from "./aem.js";
+
+const placeholder =  await fetchPlaceholders()
+const script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = placeholder.genericScript;
+script.async = true;
+document.body.appendChild(script);
