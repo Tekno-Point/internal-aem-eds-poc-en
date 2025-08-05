604091d76f4f7bb6b70f51af0cae0c2c5078be64
Om Gupta <om.gupta@teknopoint.in>
2025-08-05 12:30:28 +0530

    Generic Script make authorable

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,8 @@
+import { fetchPlaceholders } from "./aem.js";
+
+const placeholder =  await fetchPlaceholders()
 const script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://assets.adobedtm.com/56086c44eca3/d7ef90251c90/launch-1aedac27ad88-development.min.js';
+script.src = placeholder.genericScript;
 script.async = true;
 document.body.appendChild(script);
