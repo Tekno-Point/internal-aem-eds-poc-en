b3a4882ee00545c4039361d8cee0572d555a7472
Om Gupta <om.gupta@teknopoint.in>
2025-08-06 12:54:48 +0530

    function added in delay.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,17 @@
 // add delayed functionality here
+function postMessageSpartan(name, data) {
+    try {
+        if (sessionStorage.getItem('spartanEnabled')) {
+            var spartanEvent = {
+                name: name,
+                data: data
+            };
+            window.ReactNativeWebView.postMessage(JSON.stringify(spartanEvent));
+        }
+    } catch (error) {
+        console.error("An error occurred in postMessageSpartan function:", error);
+    }
+}
+
+const data = { url: 'https://poonawalafindev--internal-aem-eds-poc-en--tekno-point.aem.live/poonawallafincorp/' };
+postMessageSpartan("share", data);
